ca8a51dd8e78a5c8edb72fef5555db5c23abd0ad
William Chang <mr.williamchang@gmail.com>
2015-11-10 17:51:15 -0800

    check in

---
+++ main.py
@@ -0,0 +1,82 @@
+import collections
+import unittest
+
+
+
+Point = collections.namedtuple('Point', ['x', 'y'])
+
+
+def GenAdjacentPoints(origin):
+    """Generates adjacent points to origin"""
+    for i in [1, 0, -1]:
+        for j in [-1, 0, 1]:
+            if i == 0 and j == 0:
+                continue
+            yield Point(origin.x + j, origin.y + i)
+
+
+def IsInBounds(point, width, height):
+    """Returns if a point is within the bounds"""
+    return 0 <= point.x < width and 0 <= point.y < height
+
+
+def FindSameColorConnectedPoints(root, data, width, height):
+    """Returns list of points connected to |root| that are the same color as root"""
+    todo = collections.deque([root])
+    seenPoints = set()
+    seenPoints.add(root)
+    sameColorPoints = set()
+    startColor = data[root.x][root.y]
+    while todo:
+        currentPoint = todo.popleft()
+        if data[currentPoint.x][currentPoint.y] == startColor:
+            sameColorPoints.add(currentPoint)
+            for adjacentPoint in GenAdjacentPoints(currentPoint):
+                if adjacentPoint not in seenPoints and \
+                       IsInBounds(adjacentPoint, width, height):
+                    seenPoints.add(adjacentPoint)
+                    todo.append(adjacentPoint)
+    return sameColorPoints
+
+
+def GetFloodFillPath(point, data):
+    """Returns list of Point's to flood fill"""
+    return FindSameColorConnectedPoints(point, data, len(data[0]), len(data))
+
+
+# TESTS ------------------------------
+
+class GenAdjacentPointsTest(unittest.TestCase):
+
+    def test_Origin(self):
+        adjacentPoints = [point for point in GenAdjacentPoints(Point(0,0))]
+        self.assertEqual([Point(-1, 1), Point(0, 1), Point(1, 1),
+                          Point(-1, 0), Point(1, 0),
+                          Point(-1, -1), Point(0, -1), Point(1, -1)],
+                         adjacentPoints)
+
+
+class FindSameColorConnectedPointsTest(unittest.TestCase):
+
+    def test_ExampleOne(self):
+        data = [
+            [0, 0, 1],
+            [1, 1, 1],
+            [0, 1, 1],
+        ]
+        self.assertEqual(FindSameColorConnectedPoints(Point(0,0), data, 3, 3),
+            set([Point(0,0), Point(0, 1)]))
+
+    def test_ExampleTwo(self):
+        data = [
+            [0, 0, 1],
+            [1, 1, 1],
+            [0, 1, 1],
+        ]
+        self.assertEqual(FindSameColorConnectedPoints(Point(1,1), data, 3, 3),
+            set([Point(1,1), Point(1, 0), Point(0, 2), Point(1, 2), Point(2, 1),
+                 Point(2, 2)]))
+
+
+if __name__ == '__main__':
+    unittest.main()
